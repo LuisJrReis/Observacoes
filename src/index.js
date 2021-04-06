da070fe6b7b40bb6441476d97913bbc540ec7acd
Luis <luisjrreis@gmail.com>
2021-04-05 22:00:11 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const observacoes = {};
-count = 0;
+const { v4: uuidv4 } = require('uuid');
+const observacoesPorLembreteId = {};
 
 //#region *** Busca todos observacoes *** 
 //:id é um placeholder
 // Exemplo: /lembretes/123456/observacoes
 app.get('/lembretes/:id/observacoes', (req, res) => {
-    res.send(observacoes);
+    res.send(observacoesPorLembreteId[req.params.id] || []);
 });
 //#endregion
 
 //#region *** Cadastra e atualiza observacoes *** 
 app.put('/lembretes/:id/observacoes', (req, res) => {
-    count++;
+    const idObs = uuidv4();
     const { texto } = req.body;
-    observacoes[count] = {count,texto};
-    res.status(201).send(observacoes[count]);
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete =
+    observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+    observacoesPorLembreteId[req.params.id] =
+    observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
 });
 //#endregion
 
